78042faa8ae0a44fab683c526fb16fc75eb29b4d
Gokul s <samigokul474@gmail.com>
2021-04-14 13:01:46 +0530

    Update file.js

--- file.js
+++ file.js
@@ -1,6 +1,7 @@
-const fs = require('fs');
+
 const path = require('path');
-const express = require('express')
+const express = require('express');
+const fs = require('fs');
 const app = express();
 const cors = require("cors");
 app.use(cors())
